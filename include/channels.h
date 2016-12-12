3a006588b7fe568a38dceba417c0e1c555361829
Simon Guindon <simon@nureality.ca>
2016-12-12 13:05:31 -0500

    Added documentation to channels.h

--- include/channels.h
+++ include/channels.h
@@ -1,3 +1,12 @@
+/*-------------------------------------------------------------------------
+ *
+ * Overview
+ *      Primary include file for libchannel. Include this file to gain
+ *      access to the libuv extended functionality.
+ *
+ *-------------------------------------------------------------------------
+ */
+
 #pragma once
 
 #ifdef __cplusplus
@@ -29,12 +38,35 @@ extern "C" {
 #define LIBCHANNELS_CALLING_CONVENTION /* nothing */
 #endif
 
+/*
+ * dispatch_type is an enum that represents which type of TCP connection
+ * load balancing strategy will be used to distribute connection
+ * processing across threads and CPU cores. A libuv event loop is created
+ * per thread.
+ *
+ * It is recommended to run a thread per physical (not hyper-threaded) CPU core
+ * to reduce context switches. libchain can perform very fast I/O on each
+ * CPU core and CPU context switches impact performance of the library
+ * significantly.
+ *
+ * DISPATCH_TYPE_IPC will distribute TCP connections to each event loop
+ * over IPC named pipes in userland.
+ *
+ * DISPATCH_TYPE_REUSEPORT will distribute TCP connections using the
+ * SP_REUSEPORT functionality in the Linux 3.9 kernel that load balanes
+ * TCP connections between sockets. libchannel creates a socket per event
+ * loop (per thread).
+ *
+ */
 typedef enum dispatch_type
 {
     DISPATCH_TYPE_IPC,
     DISPATCH_TYPE_REUSEPORT,
 };
 
+/*
+ * connection is a struct representing a TCP client connection to the server.
+ */
 typedef struct
 {
     uv_tcp_t stream;
@@ -43,4 +75,27 @@ typedef struct
     int bytes_remaining;
 } connection;
 
-LIBCHANNELS_EXTERN int uv_multi_listen(const char* address, int port, bool tcp_nodelay, unsigned int threads, enum dispatch_type dispatcher, uv_loop_t* loop, int backlog, uv_connection_cb cb, uv_alloc_cb alloc_cb);
+/*
+ * uv_multi_listen is similar to uv_listen except it allows creating event
+ * loops and listeners for each thread defined in threads.
+ *
+ * Returns 0 for success and non-zero for errors.
+ *
+ * address
+ *      The address to listen for TCP connections on.
+ * port
+ *      The port to listen for TCP connections on.
+ * tcp_nodelay
+ *      Whether TCP nodelay should be enabled.
+ * dispatch_type
+ *      Which strategy to use to load balance TCP connections.
+ * loop
+ *      Main application event loop.
+ * connection_backlog
+ *      How many connections can be pending in the backlog.
+ * uv_connection_cb
+ *      Callback that gets called when a new TCP connection connects.
+ * uv_alloc_cb
+ *      Callback that gets called when libuv requires memory to be allocated.
+ */
+LIBCHANNELS_EXTERN int uv_multi_listen(const char* address, int port, bool tcp_nodelay, unsigned int threads, enum dispatch_type dispatcher, uv_loop_t* loop, int connection_backlog, uv_connection_cb cb, uv_alloc_cb alloc_cb);
