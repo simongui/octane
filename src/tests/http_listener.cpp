3107e41d4b21d5ad13f42e80e0fa1ab02ffd8e18
Simon Guindon <simon@nureality.ca>
2017-01-18 10:23:44 -0500

    Restructured tests to properly capture and verify callback state.

--- src/tests/http_listener.cpp
+++ src/tests/http_listener.cpp
@@ -9,6 +9,10 @@ extern "C" {
 
 SCENARIO("http_listener receives http requests", "[http_listener]") {
     GIVEN("An empty buffer") {
+        // Static state required to capture information from callbacks.
+        static int num_requests = 0;
+        static http_request** reqs;
+
         // Initialize octane types.
         http_connection* connection = new_http_connection();
         http_listener* listener = new_http_listener();
@@ -61,26 +65,95 @@ SCENARIO("http_listener receives http requests", "[http_listener]") {
                     "Accept: */*\r\n" \
                     "\r\n";
 
-            THEN("2 http request is parsed") {
+            THEN("2 http requests are parsed") {
                 listener->request_cb = [](
                         http_connection* connection,
                         http_request** requests,
                         int number_of_requests) {
 
-                    REQUIRE(number_of_requests == 2);
-
-                    // Verify the first request.
-                    REQUIRE(sdscmp(requests[0]->path, sdsnew("/plaintext")) == 0);
-                    REQUIRE(sdscmp(requests[0]->method, sdsnew("GET")) == 0);
-                    REQUIRE(requests[0]->version == 0);
-
-                    // Verify the second request.
-                    REQUIRE(sdscmp(requests[1]->path, sdsnew("/json")) == 0);
-                    REQUIRE(sdscmp(requests[1]->method, sdsnew("POST")) == 0);
-                    REQUIRE(requests[1]->version == 1);
-
+                    num_requests = number_of_requests;
+                    reqs = requests;
                 };
                 uv_stream_on_read(stream, strlen(buffer->base), buffer);
+
+                REQUIRE(num_requests == 2);
+
+                // Verify the first request.
+                REQUIRE(sdscmp(reqs[0]->path, sdsnew("/plaintext")) == 0);
+                REQUIRE(sdscmp(reqs[0]->method, sdsnew("GET")) == 0);
+                REQUIRE(reqs[0]->version == 0);
+
+                // Verify the second request.
+                REQUIRE(sdscmp(reqs[1]->path, sdsnew("/json")) == 0);
+                REQUIRE(sdscmp(reqs[1]->method, sdsnew("POST")) == 0);
+                REQUIRE(reqs[1]->version == 1);
+            }
+        }
+
+        WHEN("1 complete and 1 incomplete http request is received in the buffer") {
+            // Request new buffer.
+            uv_stream_on_alloc(client, suggested_size, buffer);
+
+            buffer->base = "GET /plaintext HTTP/1.0\r\n" \
+                    "Host: localhost:8000\r\n" \
+                    "User-Agent: curl/7.49.1\r\n" \
+                    "Accept: */*\r\n" \
+                    "\r\n" \
+                    "POST /json";
+
+            THEN("1 http request is parsed") {
+                listener->request_cb = [](
+                        http_connection* connection,
+                        http_request** requests,
+                        int number_of_requests) {
+
+                    num_requests = number_of_requests;
+                    reqs = requests;
+                };
+                uv_stream_on_read(stream, strlen(buffer->base), buffer);
+
+                REQUIRE(num_requests == 1);
+
+                // Verify the first request.
+                REQUIRE(sdscmp(reqs[0]->path, sdsnew("/plaintext")) == 0);
+                REQUIRE(sdscmp(reqs[0]->method, sdsnew("GET")) == 0);
+                REQUIRE(reqs[0]->version == 0);
+
+
+                AND_WHEN("the rest of the incomplete request is received in the buffer") {
+                    num_requests = 0;
+
+                    // Request new buffer. Octane should detect a partial request
+                    // and re-use previous buffer from the last parsed offset.
+                    uv_buf_t* buffer3 = (uv_buf_t*)malloc(sizeof(uv_buf_t));
+                    uv_stream_on_alloc(client, suggested_size, buffer3);
+
+                    buffer3->base = " HTTP/1.1\r\n" \
+                            "Host: localhost:8000\r\n" \
+                            "User-Agent: curl/7.49.1\r\n" \
+                            "Accept: */*\r\n" \
+                            "\r\n";
+
+                    THEN("the completed request is parsed") {
+                        listener->request_cb = [](
+                                http_connection* connection,
+                                http_request** requests,
+                                int number_of_requests) {
+
+                            num_requests = number_of_requests;
+                            reqs = requests;
+
+                        };
+                        uv_stream_on_read(stream, strlen(buffer3->base), buffer3);
+
+                        REQUIRE(num_requests == 1);
+
+                        // Verify the first request.
+                        REQUIRE(sdscmp(reqs[0]->path, sdsnew("/json")) == 0);
+                        REQUIRE(sdscmp(reqs[0]->method, sdsnew("POST")) == 0);
+                        REQUIRE(reqs[0]->version == 1);
+                    }
+                }
             }
         }
     }
